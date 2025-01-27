181eb277d529c1e04b41186e69a15b9e474532aa
ritik-mewada <ritikmewada@gmail.com>
2025-01-27 15:44:19 -0500

    first test setup

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./login";
+
+describe("Login Page", () => {
+  it("should render with required fields", () => {
+    render(<LoginPage />);
+    // getBy -> throws error if not found
+    // queryBy -> returns null if not found
+    // findBy -> async version of getBy
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Forgot password")).toBeInTheDocument();
+  });
+});
