b529ec01cfa9f1258d601eb7f4cb4a8af260c0db
ritik-mewada <ritikmewada@gmail.com>
2025-01-28 15:03:04 -0500

    add login form

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "antd/dist/reset.css";
 import { router } from "./router";
 import "./index.css";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#F65F42",
+          colorLink: "#F65F42",
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </StrictMode>
 );
