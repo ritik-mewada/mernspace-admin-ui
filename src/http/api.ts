655dacb146f978bada3e34cb18067f458e09208d
ritik-mewada <ritikmewada@gmail.com>
2025-02-03 15:29:10 -0500

    add self endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,3 +3,5 @@ import { api } from "./client";
 
 export const login = async (credentials: Credentials) =>
   api.post("/auth/login", credentials);
+
+export const self = async () => api.get("/auth/self");
