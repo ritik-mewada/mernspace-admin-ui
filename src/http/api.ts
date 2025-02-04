b32dd2a73edc2411316916550e15423096847825
ritik-mewada <ritikmewada@gmail.com>
2025-02-03 22:31:41 -0500

    add auto refresh token

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,7 +2,7 @@ import { Credentials } from "../types";
 import { api } from "./client";
 
 export const login = async (credentials: Credentials) =>
-  api.post("/auth/login", credentials);
+    api.post("/auth/login", credentials);
 
 export const self = async () => api.get("/auth/self");
 
--- src/http/client.ts
+++ src/http/client.ts
@@ -1,10 +1,44 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+    baseURL: import.meta.env.VITE_BACKEND_API_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
 });
+
+const refreshToken = async () => {
+    await axios.post(
+        `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+        {},
+        {
+            withCredentials: true,
+        }
+    );
+};
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            try {
+                originalRequest._retry = true;
+                await refreshToken();
+                return api.request({
+                    ...originalRequest,
+                    headers: { ...originalRequest.headers },
+                });
+            } catch (err) {
+                useAuthStore.getState().logout();
+                return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
