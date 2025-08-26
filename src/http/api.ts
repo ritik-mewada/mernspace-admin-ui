45ddf779e24b8004ba108481c0bcc4d2073653c3
ritik-mewada <ritikmewada@gmail.com>
2025-08-26 17:20:27 -0400

    integrate pagination

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,15 +2,16 @@ import { CreateUserData, Credentials } from "../types";
 import { api } from "./client";
 
 export const login = async (credentials: Credentials) =>
-    api.post("/auth/login", credentials);
+  api.post("/auth/login", credentials);
 
 export const self = async () => api.get("/auth/self");
 
 export const logout = async () => api.post("/auth/logout");
 
-export const getUsers = async () => api.get("/users");
+export const getUsers = async (queryString: string) =>
+  api.get(`/users?${queryString}`);
 
 export const getTenants = async () => api.get("/tenants");
 
 export const createUser = async (user: CreateUserData) =>
-    api.post("/users", user);
+  api.post("/users", user);
--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -8,145 +8,168 @@ import { useAuthStore } from "../../store";
 import UsersFilter from "./UsersFilter";
 import { useState } from "react";
 import UserForm from "./forms/UserForm";
+import { PER_PAGE } from "../../constants";
 
 const columns = [
-    {
-        title: "ID",
-        dataIndex: "id",
-        key: "id",
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
     },
-    {
-        title: "Name",
-        dataIndex: "firstName",
-        key: "firstName",
-        render: (_text: string, record: User) => {
-            return (
-                <div>
-                    {record.firstName} {record.lastName}
-                </div>
-            );
-        },
-    },
-    {
-        title: "Role",
-        dataIndex: "role",
-        key: "role",
-    },
-    {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
-    },
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
 ];
 
 const UserPage = () => {
-    const [form] = Form.useForm();
-    const queryClient = useQueryClient();
-    const {
-        token: { colorBgLayout },
-    } = theme.useToken();
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const {
-        data: users,
-        isLoading,
-        error,
-        isError,
-    } = useQuery({
-        queryKey: ["users"],
-        queryFn: () => {
-            return getUsers().then((response) => response.data);
-        },
-    });
+  const [form] = Form.useForm();
+  const queryClient = useQueryClient();
+  const {
+    token: { colorBgLayout },
+  } = theme.useToken();
+  const [queryParams, setQueryParams] = useState({
+    perPage: PER_PAGE,
+    currentPage: 1,
+  });
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const {
+    data: users,
+    isLoading,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["users", queryParams],
+    queryFn: () => {
+      const queryString = new URLSearchParams(
+        queryParams as unknown as Record<string, string>
+      ).toString();
+      return getUsers(queryString).then((response) => response.data);
+    },
+  });
 
-    const { user } = useAuthStore();
+  const { user } = useAuthStore();
 
-    const { mutate: userMutate } = useMutation({
-        mutationKey: ["user"],
-        mutationFn: async (data: CreateUserData) =>
-            createUser(data).then((res) => res.data),
-        onSuccess: async () => {
-            queryClient.invalidateQueries({ queryKey: ["users"] });
-        },
-    });
+  const { mutate: userMutate } = useMutation({
+    mutationKey: ["user"],
+    mutationFn: async (data: CreateUserData) =>
+      createUser(data).then((res) => res.data),
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+  });
 
-    if (user?.role !== "admin") {
-        return <Navigate to="/" replace={true} />;
-    }
+  if (user?.role !== "admin") {
+    return <Navigate to="/" replace={true} />;
+  }
 
-    const onHandleSubmit = async () => {
-        await form.validateFields();
-        await userMutate(form.getFieldsValue());
-        form.resetFields();
-        setDrawerOpen(false);
-    };
-    return (
-        <>
-            <Space direction="vertical" size="large" style={{ width: "100%" }}>
-                <Breadcrumb
-                    separator={<RightOutlined />}
-                    items={[
-                        {
-                            title: <Link to="/">Dashboard</Link>,
-                        },
-                        {
-                            title: "Users",
-                        },
-                    ]}
-                />
-                {isLoading && <p>Loading users...</p>}
-                {isError && <p>Error loading users: {error.message}</p>}
+  const onHandleSubmit = async () => {
+    await form.validateFields();
+    await userMutate(form.getFieldsValue());
+    form.resetFields();
+    setDrawerOpen(false);
+  };
+  return (
+    <>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            {
+              title: <Link to="/">Dashboard</Link>,
+            },
+            {
+              title: "Users",
+            },
+          ]}
+        />
+        {isLoading && <p>Loading users...</p>}
+        {isError && <p>Error loading users: {error.message}</p>}
 
-                <UsersFilter
-                    onFilterChange={(filterName, filterValue) => {
-                        console.log(
-                            `Filter changed: ${filterName} = ${filterValue}`
-                        );
-                    }}
-                >
-                    <Button
-                        type="primary"
-                        icon={<PlusOutlined />}
-                        onClick={() => setDrawerOpen(true)}
-                    >
-                        Create User
-                    </Button>
-                </UsersFilter>
+        <UsersFilter
+          onFilterChange={(filterName, filterValue) => {
+            console.log(`Filter changed: ${filterName} = ${filterValue}`);
+          }}
+        >
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setDrawerOpen(true)}
+          >
+            Create User
+          </Button>
+        </UsersFilter>
 
-                <Table columns={columns} dataSource={users?.data} rowKey="id" />
+        <Table
+          columns={columns}
+          dataSource={users?.data}
+          rowKey="id"
+          pagination={{
+            total: users?.total,
+            pageSize: queryParams.perPage,
+            current: queryParams.currentPage,
+            onChange: (page) => {
+              setQueryParams((prev) => {
+                return {
+                  ...prev,
+                  currentPage: page,
+                };
+              });
+            },
+          }}
+        />
 
-                <Drawer
-                    title="Create user"
-                    width={720}
-                    styles={{ body: { background: colorBgLayout } }}
-                    open={drawerOpen}
-                    destroyOnClose
-                    onClose={() => {
-                        form.resetFields();
-                        setDrawerOpen(false);
-                    }}
-                    extra={
-                        <Space>
-                            <Button
-                                onClick={() => {
-                                    form.resetFields();
-                                    setDrawerOpen(false);
-                                }}
-                            >
-                                Cancel
-                            </Button>
-                            <Button type="primary" onClick={onHandleSubmit}>
-                                Submit
-                            </Button>
-                        </Space>
-                    }
-                >
-                    <Form layout="vertical" form={form}>
-                        <UserForm />
-                    </Form>
-                </Drawer>
+        <Drawer
+          title="Create user"
+          width={720}
+          styles={{ body: { background: colorBgLayout } }}
+          open={drawerOpen}
+          destroyOnClose
+          onClose={() => {
+            form.resetFields();
+            setDrawerOpen(false);
+          }}
+          extra={
+            <Space>
+              <Button
+                onClick={() => {
+                  form.resetFields();
+                  setDrawerOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button type="primary" onClick={onHandleSubmit}>
+                Submit
+              </Button>
             </Space>
-        </>
-    );
+          }
+        >
+          <Form layout="vertical" form={form}>
+            <UserForm />
+          </Form>
+        </Drawer>
+      </Space>
+    </>
+  );
 };
 
 export default UserPage;
