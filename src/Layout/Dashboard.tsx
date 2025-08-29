95d0c92bf9b6e585bfead00e8d0103b38d89ecc4
ritik-mewada <ritikmewada@gmail.com>
2025-08-29 12:19:57 -0400

    add intented url redirect

--- src/Layout/Dashboard.tsx
+++ src/Layout/Dashboard.tsx
@@ -1,16 +1,16 @@
-import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store";
 import Icon, { BellFilled } from "@ant-design/icons";
 import {
-    Avatar,
-    Badge,
-    Dropdown,
-    Flex,
-    Layout,
-    Menu,
-    Space,
-    Tag,
-    theme,
+  Avatar,
+  Badge,
+  Dropdown,
+  Flex,
+  Layout,
+  Menu,
+  Space,
+  Tag,
+  theme,
 } from "antd";
 import { useState } from "react";
 import Logo from "../components/icons/Logo";
@@ -25,141 +25,136 @@ import { logout } from "../http/api";
 const { Sider, Header, Content, Footer } = Layout;
 
 const getMenuItems = (role: string) => {
-    const baseItems = [
-        {
-            key: "/",
-            icon: <Icon component={Home} />,
-            label: <NavLink to="/">Home</NavLink>,
-        },
-        {
-            key: "/restaurants",
-            icon: <Icon component={foodIcon} />,
-            label: <NavLink to="/restaurants">Restaurants</NavLink>,
-        },
-        {
-            key: "/products",
-            icon: <Icon component={BasketIcon} />,
-            label: <NavLink to="/products">Products</NavLink>,
-        },
-        {
-            key: "/promos",
-            icon: <Icon component={GiftIcon} />,
-            label: <NavLink to="/promos">Promos</NavLink>,
-        },
-    ];
+  const baseItems = [
+    {
+      key: "/",
+      icon: <Icon component={Home} />,
+      label: <NavLink to="/">Home</NavLink>,
+    },
+    {
+      key: "/restaurants",
+      icon: <Icon component={foodIcon} />,
+      label: <NavLink to="/restaurants">Restaurants</NavLink>,
+    },
+    {
+      key: "/products",
+      icon: <Icon component={BasketIcon} />,
+      label: <NavLink to="/products">Products</NavLink>,
+    },
+    {
+      key: "/promos",
+      icon: <Icon component={GiftIcon} />,
+      label: <NavLink to="/promos">Promos</NavLink>,
+    },
+  ];
 
-    if (role === "admin") {
-        const menus = [...baseItems];
-        menus.splice(1, 0, {
-            key: "/users",
-            icon: <Icon component={UserIcon} />,
-            label: <NavLink to="/users">Users</NavLink>,
-        });
+  if (role === "admin") {
+    const menus = [...baseItems];
+    menus.splice(1, 0, {
+      key: "/users",
+      icon: <Icon component={UserIcon} />,
+      label: <NavLink to="/users">Users</NavLink>,
+    });
 
-        return menus;
-    }
-    return baseItems;
+    return menus;
+  }
+  return baseItems;
 };
 
 const Dashboard = () => {
-    const { logout: logoutFromStore } = useAuthStore();
-    const { mutate: logoutMutate } = useMutation({
-        mutationKey: ["logout"],
-        mutationFn: logout,
-        onSuccess: async () => {
-            logoutFromStore();
-            return;
-        },
-    });
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
-    const { user } = useAuthStore();
+  const location = useLocation();
+  const { logout: logoutFromStore } = useAuthStore();
+  const { mutate: logoutMutate } = useMutation({
+    mutationKey: ["logout"],
+    mutationFn: logout,
+    onSuccess: async () => {
+      logoutFromStore();
+      return;
+    },
+  });
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const { user } = useAuthStore();
 
-    if (user === null) {
-        return <Navigate to="/auth/login" replace={true} />;
-    }
-    const items = getMenuItems(user?.role);
-
+  if (user === null) {
     return (
-        <div>
-            <Layout style={{ minHeight: "100vh" }}>
-                <Sider
-                    theme="light"
-                    collapsible
-                    collapsed={collapsed}
-                    onCollapse={(value) => setCollapsed(value)}
-                >
-                    <div className="logo">
-                        <Logo />
-                    </div>
-                    <Menu
-                        theme="light"
-                        defaultSelectedKeys={["/"]}
-                        mode="inline"
-                        items={items}
-                    />
-                </Sider>
-                <Layout>
-                    <Header
-                        style={{
-                            paddingLeft: "16px",
-                            paddingRight: "16px",
-                            background: colorBgContainer,
-                        }}
-                    >
-                        <Flex
-                            gap="middle"
-                            align="center"
-                            justify="space-between"
-                        >
-                            <Tag
-                                color="volcano"
-                                style={{ borderRadius: "50px" }}
-                            >
-                                {user.role === "admin"
-                                    ? "You are an admin"
-                                    : user.tenant?.name}
-                            </Tag>
-                            <Space size={16}>
-                                <Badge dot>
-                                    <BellFilled />
-                                </Badge>
-                                <Dropdown
-                                    menu={{
-                                        items: [
-                                            {
-                                                key: "logout",
-                                                label: "Logout",
-                                                onClick: () => logoutMutate(),
-                                            },
-                                        ],
-                                    }}
-                                    placement="bottomRight"
-                                >
-                                    <Avatar
-                                        style={{
-                                            backgroundColor: "#fde3cf",
-                                            color: "#f56a00",
-                                        }}
-                                    >
-                                        U
-                                    </Avatar>
-                                </Dropdown>
-                            </Space>
-                        </Flex>
-                    </Header>
-                    <Content style={{ margin: "24px" }}>
-                        <Outlet />
-                    </Content>
-                    <Footer style={{ textAlign: "center" }}>
-                        mernspace pizza shop
-                    </Footer>
-                </Layout>
-            </Layout>
-        </div>
+      <Navigate
+        to={`/auth/login?returnTo=${location.pathname}`}
+        replace={true}
+      />
     );
+  }
+  const items = getMenuItems(user?.role);
+
+  return (
+    <div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header
+            style={{
+              paddingLeft: "16px",
+              paddingRight: "16px",
+              background: colorBgContainer,
+            }}
+          >
+            <Flex gap="middle" align="center" justify="space-between">
+              <Tag color="volcano" style={{ borderRadius: "50px" }}>
+                {user.role === "admin" ? "You are an admin" : user.tenant?.name}
+              </Tag>
+              <Space size={16}>
+                <Badge dot>
+                  <BellFilled />
+                </Badge>
+                <Dropdown
+                  menu={{
+                    items: [
+                      {
+                        key: "logout",
+                        label: "Logout",
+                        onClick: () => logoutMutate(),
+                      },
+                    ],
+                  }}
+                  placement="bottomRight"
+                >
+                  <Avatar
+                    style={{
+                      backgroundColor: "#fde3cf",
+                      color: "#f56a00",
+                    }}
+                  >
+                    U
+                  </Avatar>
+                </Dropdown>
+              </Space>
+            </Flex>
+          </Header>
+          <Content style={{ margin: "24px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>mernspace pizza shop</Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
 };
 
 export default Dashboard;
